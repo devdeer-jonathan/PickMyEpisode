165c072edb6540ab15c7691da0f9a928fbb3c10d
Jonathan Lenz <jonathan.lenz@devdeer.com>
2024-08-17 17:43:02 +0200

    basic project structure

---
+++ src/Ui/Ui.Console/App.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Ui.Console
+{
+    using Console = System.Console;
+
+    /// <summary>
+    /// Contains the application code for the console app.
+    /// </summary>
+    public class App
+    {
+        private readonly ILogger<App> _logger;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="logger">The logger to use.</param>
+        public App(ILogger<App> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Represents the program logic of the console app.
+        /// </summary>
+        /// <param name="args">The command line arguments passed to the app at startup.</param>
+        /// <returns>0 if the app ran succesfully otherwise 1.</returns>
+        public Task<int> StartAsync(string[] args)
+        {
+            _logger.LogInformation("Hello from the logger.");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hit any key to exit.");
+            Console.ReadKey();
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/Ui/Ui.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Ui.Console;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        // TODO add your service dependencies here
+        services.AddSingleton<App>();
+    });
+var app = builder.Build();
+return await app.Services.GetRequiredService<App>().StartAsync(Environment.GetCommandLineArgs());
